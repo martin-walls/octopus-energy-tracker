8b6a8284a8c1344c4196e27343a00502c367f13a
Martin Walls <martin_w@optimove.com>
2025-02-25 23:03:56 +0000

    get octopus auth API call to work
    deserialise response of octopus auth
    
    get api key from env var

---
+++ octopus.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const octopusBaseUrl = "https://api.octopus.energy/v1/graphql/"
+
+var httpClient = &http.Client{}
+
+type Query struct {
+	Query     string         `json:"query"`
+	Variables map[string]any `json:"variables"`
+}
+
+func query(q Query) ([]byte, error) {
+	body, err := json.Marshal(q)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(string(body))
+
+	request, err := http.NewRequest(http.MethodPost, octopusBaseUrl, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", "application/json")
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	responseBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseBytes, nil
+}
+
+type ObtainKrakenTokenResponse struct {
+	Token string `json:"token"`
+	RefreshToken string `json:"refreshToken"`
+	RefreshExpiresIn int `json:"refreshExpiresIn"`
+}
+
+func auth() error {
+	apiKey := os.Getenv("OCTOPUS_API_KEY")
+
+	q := Query{
+		Query: `mutation ObtainKrakenToken($input: ObtainJSONWebTokenInput!) {
+			obtainKrakenToken(input: $input) {
+				token
+				refreshToken
+				refreshExpiresIn
+			}
+		}`,
+		Variables: map[string]any{
+			"input": struct {
+				APIKey string
+			}{
+				APIKey: apiKey,
+			},
+		},
+	}
+
+	responseBytes, err := query(q)
+	if err != nil {
+		return err
+	}
+
+	response := struct {
+		Data struct {
+			ObtainKrakenToken ObtainKrakenTokenResponse `json:"obtainKrakenToken"`
+		} `json:"data"`
+	}{}
+
+	err = json.Unmarshal(responseBytes, &response)
+	if err != nil {
+		return err
+	}
+
+	log.Println(response.Data.ObtainKrakenToken.RefreshToken)
+	log.Println(response.Data.ObtainKrakenToken.RefreshExpiresIn)
+
+	return nil
+}
